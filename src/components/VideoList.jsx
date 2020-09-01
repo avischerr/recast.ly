a23de6f1dcfd63219c492c9db8a28d13d7dc5d3a
Arielle Scherr <ariellescherr@gmail.com>
2020-09-01 17:07:54 -0500

    Most tests passing, failing one in video player and one in app

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,15 +1,16 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+var VideoList = ({videos}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map((video) => (
+      <VideoListEntry
+        video={video}
+        key={video.id.videoId}
+      />
+    )
+    )}
   </div>
 );
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
@@ -17,3 +18,10 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+/* <div>[{props.videoEntry}]</div>
+<div>{props.videoEntry}</div>
+<div>{props.videoEntry}</div>
+<div>{props.videoEntry}</div>
+<div>{props.videoEntry}</div> */
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
